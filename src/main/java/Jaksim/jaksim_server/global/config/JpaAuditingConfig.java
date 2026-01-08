3c5ec90547a8164f6466d392669fb5ff240f7a4f
seungmo <boorue03@gmail.com>
2026-01-09 02:04:50 +0900

    feat:기본JPA 클래스 및 domain model 기본할당 인터페이스 추가

---
+++ src/main/java/Jaksim/jaksim_server/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package Jaksim.jaksim_server.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
