448df67537bb3c123ceef0d8c61e962ae2e5f733
TakioN <jungbeen0724@gmail.com>
2026-01-09 14:07:53 +0900

    feat: implement getting user stat logic

---
+++ src/main/java/Jaksim/jaksim_server/controller/user/UserController.java
@@ -0,0 +1,29 @@
+package Jaksim.jaksim_server.controller.user;
+
+import Jaksim.jaksim_server.dto.user.UserResponse;
+import Jaksim.jaksim_server.global.response.CommonResponse;
+import Jaksim.jaksim_server.service.user.UserStatService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserStatService userService;
+
+    @Operation(
+            summary = "사용자 달성 정보 가져오기",
+            description = "사용자의 완료 루프, 연속 달성 가져오기, 달성률 가져오기"
+    )
+    @GetMapping("/user")
+    public ResponseEntity<CommonResponse<UserResponse>> getUserStats() {
+        UserResponse response = userService.getUserStats();
+        return ResponseEntity.ok(CommonResponse.success(response));
+    }
+}
--- src/main/java/Jaksim/jaksim_server/global/exception/ErrorCode.java
+++ src/main/java/Jaksim/jaksim_server/global/exception/ErrorCode.java
@@ -3,7 +3,7 @@ package Jaksim.jaksim_server.global.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-    // temp code
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.");
 
     private final HttpStatus status;
