afe29e661ebf229f1888dc897c049f347dd4441e
TakioN <jungbeen0724@gmail.com>
2026-01-09 16:13:09 +0900

    refactor: refactor folder structure ranged by domain

--- src/main/java/Jaksim/jaksim_server/repository/user/UserStatRepository.java
+++
@@ -1,9 +0,0 @@
-package Jaksim.jaksim_server.repository.user;
-
-import Jaksim.jaksim_server.domain.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserStatRepository extends JpaRepository<User, Long> {
-}
