373ea17dabf238267c064d5c90683377faf23b13
TakioN <jungbeen0724@gmail.com>
2026-01-09 19:13:02 +0900

    fix: remove user controller and user service

--- src/main/java/Jaksim/jaksim_server/domain/user/controller/UserController.java
+++ src/main/java/Jaksim/jaksim_server/domain/user/controller/UserController.java
@@ -2,7 +2,6 @@ package Jaksim.jaksim_server.domain.user.controller;
 
 import Jaksim.jaksim_server.domain.user.dto.UserResponse;
 import Jaksim.jaksim_server.global.response.CommonResponse;
-import Jaksim.jaksim_server.domain.user.service.UserStatService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +14,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserStatService userService;
-
-    @Operation(
-            summary = "사용자 달성 정보 가져오기",
-            description = "사용자의 완료 루프, 연속 달성 가져오기, 달성률 가져오기"
-    )
-    @GetMapping("/user")
-    public ResponseEntity<CommonResponse<UserResponse>> getUserStats() {
-        UserResponse response = userService.getUserStats();
-        return ResponseEntity.ok(CommonResponse.success(response));
-    }
 }
